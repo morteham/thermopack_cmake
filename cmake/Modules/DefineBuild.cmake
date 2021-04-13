8b0222220884e5cf313b05a5cc42e1c5ece2eef6
Morten Hammer <morten.hammer@sintef.no>
2021-04-13 21:28:15 +0200

    Added command to run python script after copying dynamic library

--- cmake/Modules/DefineBuild.cmake
+++ cmake/Modules/DefineBuild.cmake
@@ -16,17 +16,16 @@ endforeach()
 # Define the executable in terms of the source files
 ADD_LIBRARY(${LIBTHERMOPACK_STATIC} STATIC ${LIB_SOURCES})
 IF(MSVC)
-	ADD_LIBRARY(${LIBTHERMOPACK} SHARED ${LIB_SOURCES})
-	SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} /libs:static /threads")
-	SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /DEF:${CMAKE_SOURCE_DIR}/MSVStudio/thermopack.def")
+  ADD_LIBRARY(${LIBTHERMOPACK} SHARED ${LIB_SOURCES})
+  SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} /libs:static /threads")
+  SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /DEF:${CMAKE_SOURCE_DIR}/MSVStudio/thermopack.def")
 ELSE(MSVC)
-	IF(APPLE)
-		SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/libthermopack_export.symbols)
-	ELSE(APPLE)
-		SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/libthermopack_export.version)
-	ENDIF(APPLE)
-ADD_LIBRARY(${LIBTHERMOPACK} SHARED $<TARGET_OBJECTS:${LIBTHERMOPACK_STATIC}> )
-	SET_TARGET_PROPERTIES(${LIBTHERMOPACK} PROPERTIES LINK_DEPENDS ${LINKER_SCRIPT})
+  IF(APPLE)
+    SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -exported_symbols_list ${CMAKE_SOURCE_DIR}/libthermopack_export.symbols")
+  ELSE(APPLE)
+    SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version-script=${CMAKE_SOURCE_DIR}/libthermopack_export.version")
+  ENDIF(APPLE)
+  ADD_LIBRARY(${LIBTHERMOPACK} SHARED $<TARGET_OBJECTS:${LIBTHERMOPACK_STATIC}>)
 ENDIF(MSVC)
 ADD_EXECUTABLE(${THERMOPACKEXE} ${SRC}/thermopack.f90)
 TARGET_INCLUDE_DIRECTORIES(${LIBTHERMOPACK} PRIVATE ${CMAKE_SOURCE_DIR}/addon/trend_interface/include)
@@ -36,9 +35,19 @@ TARGET_INCLUDE_DIRECTORIES(${LIBTHERMOPACK_STATIC} PRIVATE ${CMAKE_SOURCE_DIR}/a
 ADD_CUSTOM_TARGET(${PYCTHERMOPACK} ALL
   COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${LIBTHERMOPACK}>          ${CMAKE_SOURCE_DIR}/addon/pycThermopack/pyctp
   # Output Message
-  COMMENT "Copying libthermopack shared library from '${LIB}' to '${CMAKE_SOURCE_DIR}/addon/pycThermopack/pyctp'" VERBATIM
-)
+  COMMENT "Copying libthermopack shared library from '${LIB}' to '${CMAKE_SOURCE_DIR}/addon/pycThermopack/pyctp'"
+  VERBATIM
+  )
+
 ADD_DEPENDENCIES(${PYCTHERMOPACK} ${LIBTHERMOPACK})
+
+ADD_CUSTOM_COMMAND(TARGET ${PYCTHERMOPACK} POST_BUILD
+  COMMAND ${PYTHON_EXE} generate_platform_specifics.py
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/addon/pycThermopack/pyctp/
+  COMMENT "Detecting platform specifics for python interface"
+  VERBATIM
+  USES_TERMINAL
+  )
 
 #####################################################
 # Add the needed libraries and special compiler flags
